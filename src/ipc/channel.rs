100788fcbbf94a4993baf63c759178614ac395f4
fd <wawuta@126.com>
2021-09-24 22:45:28 +0800

    change FnMut to FnOnce

--- src/ipc/channel.rs
+++ src/ipc/channel.rs
@@ -13,6 +13,13 @@ impl Channel {
     fn new(koid: KoID) -> Self {
         Channel { koid }
     }
+    pub fn create() -> (Handle, Handle) {
+        let end0 = Channel::new(0);
+        let end1 = Channel::new(1);
+        let handle0 = Handle::new(Arc::new(Mutex::new(end0)), Rights::DUPLICATE);
+        let handle1 = Handle::new(Arc::new(Mutex::new(end1)), Rights::DUPLICATE);
+        (handle0, handle1)
+    }
 }
 
 impl KernelObject for Channel {
@@ -25,26 +32,15 @@ impl KernelObject for Channel {
     }
 }
 
-pub fn create() -> (Handle, Handle) {
-    let end0 = Channel::new(0);
-    let end1 = Channel::new(1);
-    let handle0 = Handle::new(Arc::new(Mutex::new(end0)), Rights::DUPLICATE);
-    let handle1 = Handle::new(Arc::new(Mutex::new(end1)), Rights::DUPLICATE);
-    (handle0, handle1)
-}
-
 #[cfg(test)]
 mod tests {
-    use crate::ipc::channel::{create, Channel};
+    use crate::ipc::channel::Channel;
     use crate::object::KernelObject;
     use crate::ZxError;
 
     #[test]
-    fn aaa() {}
-
-    #[test]
     fn is_work() {
-        let (handle0, handle1) = create();
+        let (handle0, handle1) = Channel::create();
         handle0.do_mut(|ch: &mut Channel| {
             assert_eq!(0u64, ch.id());
             ZxError::OK
